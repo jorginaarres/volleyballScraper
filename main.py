a2e16fd32cd3c778fe5b619f0f8fb17c35c7a60d
1203217 <jorgina.arres@est.fib.upc.edu>
2020-11-01 19:03:56 +0100

    Add table tr scraping and finded url to get <script> data

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from urllib.request import urlopen
+import requests
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
 url = "http://www.rfevb.com/primera-division-femenina-grupo-a-clasificacion"
-page = urlopen(url)
-html = page.read().decode("utf-8")
-soup = BeautifulSoup(html, "html.parser")
-print(soup)
+# utils url : https://intranet.rfevb.com/rfevbcom/includes-html/competiciones/clasificacion321-conEncuentros-datos.php?IdCampeonato=4059&Jornada=3
+# Make a GET request to fetch the raw HTML content
+html_content = requests.get(url).text
+
+# Parse the html content
+soup = BeautifulSoup(html_content, "lxml")
+print(soup.prettify()) # print the parsed data of html
+
+content = requests.get("https://intranet.rfevb.com/rfevbcom/includes-html/competiciones/clasificacion321-conEncuentros-datos.php?IdCampeonato="+'4059'+"&Jornada="+'4').content
+
+all_trs = BeautifulSoup(requests.get("https://intranet.rfevb.com/rfevbcom/includes-html/competiciones/clasificacion321-conEncuentros-datos.php?IdCampeonato="+'4059'+"&Jornada="+'4').content).findAll('tr')
